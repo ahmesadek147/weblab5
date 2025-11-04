5376e5be668ac9de451d78e201fcc51fa36e37e5
ahmesadek147 <as6694874@gmail.com>
2025-11-04 21:57:46 +0200

    Add files via upload

---
+++ src/components/movieform.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function MovieForm({ addMovie }) {
+  const [title, setTitle] = useState("");
+  const [review, setReview] = useState("");
+  const [rating, setRating] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title || rating === 0) return alert("Please enter a title & rating!");
+
+    addMovie({
+      id: Date.now(),
+      title,
+      review,
+      rating,
+    });
+
+    setTitle("");
+    setReview("");
+    setRating(0);
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="🎥 Movie title..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <textarea
+        placeholder="💬 Write a short review..."
+        value={review}
+        onChange={(e) => setReview(e.target.value)}
+      ></textarea>
+
+      <div className="stars">
+        {[1, 2, 3, 4, 5].map((star) => (
+          <span
+            key={star}
+            onClick={() => setRating(star)}
+            style={{
+              color: star <= rating ? "#ffcc00" : "#ccc",
+            }}
+          >
+            ⭐
+          </span>
+        ))}
+      </div>
+
+      <button type="submit">Add Movie</button>
+    </form>
+  );
+}
